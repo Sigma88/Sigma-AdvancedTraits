857d7c0f6fd9646e9700bb9a5f8de6a8434539a9
Sigma88 <sigmamodexpansions@outlook.com>
2019-07-04 17:12:05 +0200

    AdvancedRepair

---
+++ [Source]/SigmaAdvancedTraits/AdvancedRepair.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using ModuleWheels;
+
+
+namespace SigmaAdvancedTraitsPlugin
+{
+    internal class ModuleVesselRepair : PartModule
+    {
+        [KSPEvent(guiActiveUnfocused = false, guiActive = false, guiActiveEditor = false, guiName = "Repair Vessel")]
+        public void RepairAll()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                Debug.Log("RepairAll", "Vessel =  " + vessel);
+                if (vessel != null)
+                {
+                    ModuleAdvancedRepair[] MAR = vessel.Parts.Where(p => p?.GetComponent<ModuleAdvancedRepair>() != null).Select(p => p?.GetComponent<ModuleAdvancedRepair>()).ToArray();
+                    Debug.Log("RepairAll", "Found " + MAR?.Length + " modules of type <ModuleAdvancedRepair>");
+
+                    for (int i = 0; i < MAR?.Length; i++)
+                    {
+                        MAR[i].Repair();
+                    }
+
+                    ModuleWheelDamage[] MWB = vessel.Parts.Where(p => p?.GetComponent<ModuleWheelDamage>() != null).Select(p => p?.GetComponent<ModuleWheelDamage>()).ToArray();
+                    Debug.Log("RepairAll", "Found " + MWB?.Length + " modules of type <ModuleWheelDamage>");
+
+                    for (int i = 0; i < MWB?.Length; i++)
+                    {
+                        if (MWB[i].isRepairable && MWB[i].isDamaged)
+                        {
+                            MWB[i].EventRepairExternal();
+                        }
+                    }
+
+                    ModuleParachute[] MP = vessel.Parts.Where(p => p?.GetComponent<ModuleParachute>() != null).Select(p => p?.GetComponent<ModuleParachute>()).ToArray();
+                    Debug.Log("RepairAll", "Found " + MP?.Length + " modules of type <ModuleParachute>");
+
+                    for (int i = 0; i < MP?.Length; i++)
+                    {
+                        if (MP[i].deploymentState == ModuleParachute.deploymentStates.CUT)
+                        {
+                            MP[i].Repack();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    internal class ModuleAdvancedRepair : PartModule
+    {
+        internal List<Collider> newCollidersList = new List<Collider>();
+
+        int retractIn = 0;
+
+        [KSPEvent(guiActiveUnfocused = false, guiActive = false, guiActiveEditor = false, guiName = "Retract Manually")]
+        public void RetractManually()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                ModuleDeployableSolarPanel MDSP = part?.GetComponent<ModuleDeployableSolarPanel>();
+
+                Debug.Log("RetractManually", "ModuleDeployableSolarPanel = " + MDSP);
+                if (MDSP != null)
+                {
+                    Debug.Log("RetractManually", "retractable = " + MDSP.retractable);
+                    if (!MDSP.retractable)
+                    {
+                        MDSP.deployState = ModuleDeployablePart.DeployState.BROKEN;
+                        Repair(0);
+                    }
+                }
+            }
+        }
+
+        [KSPEvent(guiActiveUnfocused = false, guiActive = false, guiActiveEditor = false, guiName = "Repair Part")]
+        public void Repair()
+        {
+            Repair(1);
+        }
+
+        void Repair(int cost = 1)
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                ModuleDeployablePart MDP = part?.GetComponent<ModuleDeployablePart>();
+
+                Debug.Log("Repair", "ModuleDeployablePart = " + MDP);
+                if (MDP != null)
+                {
+                    Debug.Log("Repair", "DeployState = " + MDP?.deployState);
+                    if (MDP?.deployState == ModuleDeployablePart.DeployState.BROKEN)
+                    {
+                        Debug.Log("Repair", "hasPivot = " + MDP?.hasPivot);
+                        if (MDP.hasPivot)
+                        {
+                            GameObject broken = MDP?.panelRotationTransform?.gameObject;
+                            Debug.Log("Repair", "broken = " + broken);
+                            GameObject prefab = part?.partInfo?.partPrefab?.FindModelTransform(MDP.pivotName)?.gameObject;
+                            Debug.Log("Repair", "prefab = " + prefab);
+
+                            if (broken != null && prefab != null)
+                            {
+                                // Delete breakObjects
+                                FieldInfo MDP_breakObjects = typeof(ModuleDeployablePart).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Skip(3).FirstOrDefault();
+                                Debug.Log("Repair", "MDP_breakObjects.Name = " + MDP_breakObjects?.Name + ", should be = breakObjects");
+
+                                List<GameObject> oldBreakObjects = (List<GameObject>)MDP_breakObjects?.GetValue(MDP);
+                                Debug.Log("Repair", "Destroying " + oldBreakObjects?.Count + " breakObjects");
+                                for (int i = 0; i < oldBreakObjects?.Count; i++)
+                                {
+                                    DestroyImmediate(oldBreakObjects[i]);
+                                }
+
+                                Debug.Log("Repair", "Replace breakObject with empty list");
+                                MDP_breakObjects.SetValue(MDP, new List<GameObject>());
+
+
+                                // Replace Part model
+                                Debug.Log("Repair", "Clone the prefab GameObject");
+                                GameObject clone = Instantiate(prefab);
+                                clone.name = broken.name;
+                                clone.transform.SetParent(broken.transform.parent);
+                                clone.transform.localPosition = prefab.transform.localPosition;
+                                clone.transform.localScale = prefab.transform.localScale;
+                                clone.transform.localRotation = prefab.transform.localRotation;
+                                DestroyImmediate(broken);
+                                clone.SetActive(true);
+                                Debug.Log("Repair", "clone = " + clone);
+
+
+                                // Reset panelRotationTransfrom
+                                MDP.panelRotationTransform = clone.transform;
+                                MDP.originalRotation = MDP.panelRotationTransform.localRotation;
+                                MDP.currentRotation = MDP.originalRotation;
+                                Debug.Log("Repair", "new panelRotationTransform = " + MDP.panelRotationTransform);
+
+
+                                // Reset secondaryTransform
+                                FieldInfo MDP_secondaryTransform = typeof(ModuleDeployablePart).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).FirstOrDefault();
+                                Debug.Log("Repair", "MDP_secondaryTransform.Name = " + MDP_secondaryTransform?.Name + ", should be = secondaryTransform");
+
+                                Transform secondaryTransform = part?.FindModelTransform(MDP.secondaryTransformName);
+                                MDP_secondaryTransform.SetValue(MDP, secondaryTransform);
+                                Debug.Log("Repair", "new secondaryTransform = " + secondaryTransform);
+
+
+                                // Reset ModuleDeployableSolarPanel.trackingDotTransform
+                                ModuleDeployableSolarPanel MDSP = MDP as ModuleDeployableSolarPanel;
+                                Debug.Log("Repair", "ModuleDeployablePart as ModuleDeployableSolarPanel = " + MDSP);
+                                if (MDSP != null)
+                                {
+                                    Debug.Log("Repair", "MDSP.useRaycastForTrackingDot = " + MDSP.useRaycastForTrackingDot);
+                                    if (MDSP.useRaycastForTrackingDot)
+                                    {
+                                        MDSP.trackingDotTransform = secondaryTransform;
+                                    }
+                                    else
+                                    {
+                                        MDSP.trackingDotTransform = MDSP.panelRotationTransform;
+                                    }
+                                    Debug.Log("Repair", "MDSP.trackingDotTransform = " + MDSP.trackingDotTransform);
+                                }
+
+
+                                // Destroy new colliders
+                                for (int i = 0; i < newCollidersList?.Count; i++)
+                                {
+                                    Debug.Log("Repair", "Destroying newCollider[" + i + "] = " + newCollidersList[i]);
+                                    DestroyImmediate(newCollidersList[i]);
+                                }
+                                Debug.Log("Repair", "Clearing newCollidersList(" + newCollidersList?.Count + ")");
+                                newCollidersList?.Clear();
+
+
+                                // Reset ModuleDeployablePart state
+                                Debug.Log("Repair", "MDP.GetTrackingBodyTransforms()");
+                                MDP.GetTrackingBodyTransforms();
+
+                                Debug.Log("Repair", "MDP.useAnimation = " + MDP.useAnimation);
+                                if (MDP.useAnimation)
+                                {
+                                    FieldInfo MDP_anim = typeof(ModuleDeployablePart).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Skip(1).FirstOrDefault();
+                                    Debug.Log("Repair", "MDP_anim.Name = " + MDP_anim?.Name + ", should be = anim");
+                                    MDP_anim.SetValue(MDP, part.GetComponentInChildren<Animation>(true));
+
+                                    Debug.Log("Repair", "MDP.retractable = " + MDP.retractable);
+                                    if (MDP.retractable)
+                                    {
+                                        MDP.deployState = ModuleDeployablePart.DeployState.EXTENDED;
+                                        retractIn = 2;
+                                        MDP.startFSM();
+                                        Debug.Log("Repair", "after MDP.startFSM()");
+                                    }
+                                    else
+                                    {
+                                        MDP.deployState = ModuleDeployablePart.DeployState.RETRACTED;
+                                        MDP.startFSM();
+                                        Debug.Log("Repair", "after MDP.startFSM()");
+                                    }
+
+                                    Debug.Log("Repair", "MDP.deployState = " + MDP.deployState + ", retractIn = " + retractIn);
+                                }
+                                else
+                                {
+                                    MDP.deployState = ModuleDeployablePart.DeployState.EXTENDED;
+                                    MDP.startFSM();
+                                    Debug.Log("Repair", "after MDP.startFSM()");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (retractIn > 0)
+            {
+                retractIn--;
+
+                if (retractIn == 0)
+                {
+                    part.GetComponent<ModuleDeployablePart>().Retract();
+                }
+            }
+        }
+    }
+}
---
+++ [Source]/SigmaAdvancedTraits/GameEvents.cs
@@ -0,0 +1,187 @@
+﻿using System.Linq;
+using UnityEngine;
+
+
+namespace SigmaAdvancedTraitsPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    internal class FlightEvents : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("FlightEvents", "Start");
+            GameEvents.onVesselLoaded.Add(OnVesselLoaded);
+            GameEvents.onVesselWasModified.Add(OnVesselWasModified);
+            GameEvents.onVesselChange.Add(OnVesselChange);
+        }
+
+        void OnDestroy()
+        {
+            Debug.Log("FlightEvents", "OnDestroy");
+            GameEvents.onVesselLoaded.Remove(OnVesselLoaded);
+            GameEvents.onVesselWasModified.Remove(OnVesselWasModified);
+            GameEvents.onVesselChange.Remove(OnVesselChange);
+        }
+
+        void OnVesselLoaded(Vessel vessel)
+        {
+            Debug.Log("FlightEvents.OnVesselLoaded", "vessel = " + vessel);
+            AddColliders(vessel);
+            FixPAWs(vessel);
+        }
+
+        void OnVesselWasModified(Vessel vessel)
+        {
+            Debug.Log("FlightEvents.OnVesselWasModified", "vessel = " + vessel);
+            AddColliders(vessel);
+            FixPAWs(vessel);
+        }
+
+        void OnVesselChange(Vessel vessel)
+        {
+            Debug.Log("FlightEvents.OnVesselChange", "vessel = " + vessel);
+            FixPAWs(vessel);
+        }
+
+        void AddColliders(Vessel vessel)
+        {
+            Debug.Log("FlightEvents.AddCollidersToVessel", "Vessel = " + vessel);
+            Part[] parts = vessel?.Parts?.Where(p => p?.GetComponent<ModuleDeployablePart>()?.deployState == ModuleDeployablePart.DeployState.BROKEN).ToArray();
+            Debug.Log("FlightEvents.AddCollidersToVessel", "Found " + parts?.Length + " broken deployable parts.");
+            for (int i = 0; i < parts?.Length; i++)
+            {
+                Debug.Log("FlightEvents.AddCollidersToVessel", "Part[" + i + "] = " + parts[i]);
+                AddColliders(parts[i]);
+            }
+        }
+
+        void AddColliders(Part part)
+        {
+            Debug.Log("FlightEvents.AddCollidersToPart", "Part = " + part);
+            ModuleAdvancedRepair MAR = part?.GetComponent<ModuleAdvancedRepair>();
+            Debug.Log("FlightEvents.AddCollidersToPart", "ModuleVesselRepair = " + MAR);
+
+            if (MAR != null)
+            {
+                MeshFilter[] meshes = part?.GetComponentsInChildren<MeshFilter>()?.Where(mf => mf?.GetComponent<Collider>() == null)?.ToArray();
+                Debug.Log("FlightEvents.AddCollidersToPart", "Found " + meshes?.Length + " objects without a collider.");
+                for (int i = 0; i < meshes?.Length; i++)
+                {
+                    Debug.Log("FlightEvents.AddCollidersToPart", "GameObject[" + i + "] = " + meshes[i]?.gameObject);
+                    BoxCollider bc = meshes[i].gameObject.AddOrGetComponent<BoxCollider>();
+                    MAR.newCollidersList.Add(bc);
+                    Debug.Log("FlightEvents.AddCollidersToPart", "BoxCollider = " + bc);
+                }
+            }
+        }
+
+        void FixPAWs(Vessel vessel)
+        {
+            Debug.Log("FlightEvents.FixPAWs", "Vessel = " + vessel);
+            Part[] parts = vessel?.Parts?.Where(p => p?.GetComponent<ModuleVesselRepair>() != null).ToArray();
+            Debug.Log("FlightEvents.FixPAWs", "Found " + parts?.Length + " parts with ModuleVesselRepair.");
+            for (int i = 0; i < parts?.Length; i++)
+            {
+                Debug.Log("FlightEvents.FixPAWs", "Part[" + i + "] = " + parts[i]);
+                FixPAW(parts[i]);
+            }
+        }
+
+        void FixPAW(Part part)
+        {
+            Debug.Log("FlightEvents.FixPAW", "Part = " + part);
+
+            ModuleAdvancedRepair MAR = part?.GetComponent<ModuleAdvancedRepair>();
+            Debug.Log("FlightEvents.FixPAW", "ModuleAdvancedRepair = " + MAR);
+            BaseEvent Repair = MAR?.Events?["Repair"];
+            Debug.Log("FlightEvents.FixPAW", "Repair = " + Repair);
+            BaseEvent Retract = MAR?.Events?["RetractManually"];
+            Debug.Log("FlightEvents.FixPAW", "Retract = " + Retract);
+
+            ModuleVesselRepair MVR = part.GetComponent<ModuleVesselRepair>();
+            Debug.Log("FlightEvents.FixPAW", "ModuleVesselRepair = " + MVR);
+            BaseEvent RepairAll = MVR?.Events?["RepairAll"];
+            Debug.Log("FlightEvents.FixPAW", "RepairAll = " + RepairAll);
+
+            if (Repair == null && RepairAll == null) return;
+
+            Vessel activeVessel = FlightGlobals.ActiveVessel;
+            Debug.Log("FlightEvents.FixPAW", "activeVessel = " + activeVessel);
+
+            if (activeVessel != null)
+            {
+                Debug.Log("FlightEvents.FixPAW", "activeVessel.isEVA = " + activeVessel.isEVA);
+                if (activeVessel.isEVA)
+                {
+                    ProtoCrewMember kerbal = activeVessel.Parts[0].protoModuleCrew[0];
+
+                    Debug.Log("FlightEvents.FixPAW", "kerbal = " + kerbal);
+                    if (kerbal != null)
+                    {
+                        Debug.Log("FlightEvents.FixPAW", "kerbal.experienceLevel = " + kerbal.experienceLevel);
+                        if (kerbal.careerLog.HasEntry("Training1", AdvancedTraitsButtons.AdvTraits["Engineer"][0]))
+                        {
+                            Debug.Log("FlightEvents.FixPAW", kerbal.name + " is trained in Part Repair");
+                            if (kerbal.experienceLevel >= 2)
+                            {
+                                Debug.Log("FlightEvents.FixPAW", "ModuleAdvancedRepair = " + MAR);
+                                if (MAR != null)
+                                {
+                                    ModuleDeployablePart MDP = part.GetComponent<ModuleDeployablePart>();
+                                    Debug.Log("FlightEvents.FixPAW", "ModuleDeployablePart = " + MDP);
+
+                                    Repair.guiActive =
+                                    Repair.guiActiveUnfocused =
+                                    (
+                                        (MDP is ModuleDeployableAntenna && kerbal.experienceLevel >= 2) ||
+                                        (MDP is ModuleDeployableRadiator && kerbal.experienceLevel >= 3) ||
+                                        (MDP is ModuleDeployableSolarPanel && kerbal.experienceLevel >= 4)
+                                    );
+                                    Debug.Log("FlightEvents.FixPAW", "Repair guiActive = " + Repair.guiActive);
+                                    Debug.Log("FlightEvents.FixPAW", "Repair guiActive = " + Repair.guiActive);
+
+                                    Retract.guiActive =
+                                    Retract.guiActiveUnfocused =
+                                    (
+                                        Repair.guiActiveUnfocused == true &&
+                                        MDP?.deployState == ModuleDeployablePart.DeployState.EXTENDED &&
+                                        MDP?.retractable == false
+                                    );
+                                }
+
+                                Debug.Log("FlightEvents.FixPAW", "ModuleVesselRepair = " + MVR);
+                                if (MVR != null)
+                                {
+                                    RepairAll.guiActive =
+                                    RepairAll.guiActiveUnfocused =
+                                    kerbal.experienceLevel >= 5;
+                                }
+
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            Debug.Log("FlightEvents.FixPAW", kerbal.name + " is NOT trained in Part Repair");
+                        }
+                    }
+                }
+            }
+
+            // Reset all to the default
+            if (MAR != null)
+            {
+                Repair.guiActive =
+                Repair.guiActiveUnfocused =
+                Retract.guiActive =
+                Retract.guiActiveUnfocused = false;
+            }
+
+            if (MVR != null)
+            {
+                RepairAll.guiActive =
+                RepairAll.guiActiveUnfocused = false;
+            }
+        }
+    }
+}
